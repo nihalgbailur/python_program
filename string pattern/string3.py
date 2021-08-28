1d8e0315b83e1a07b785bf6f5debd55238a5dcd2
NIHAL G BAILUR <nihalgbailur@NIHALs-MacBook-Air.local>
2021-08-28 06:36:02 +0530

    reverse word in python

---
+++ string pattern/string3.py
@@ -0,0 +1,9 @@
+# Reverse words in a given String in Python
+## initializing the string
+string = "I am software developer"
+## splitting the string on space
+words = string.split()
+## reversing the words using reversed() function
+words = list(reversed(words))
+## joining the words and printing
+print(" ".join(words))
